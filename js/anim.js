2efca2b59e6d02c5723c5dd88d1dd3afba0d029d
დავით სირაძე <69124314+davit-siradze@users.noreply.github.com>
2021-02-28 01:07:52 -0800

    animation

---
+++ js/anim.js
@@ -0,0 +1,68 @@
+TweenMax.from(".logo", 1.6, {
+    delay: 0.7,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+  });
+  
+  TweenMax.from(".navli", 1.6, {
+    delay:1.1,
+    opacity:0,
+    y:20,
+    ease: Expo.easeInOut
+  });
+  TweenMax.from(".proffile", 1.6, {
+    delay: 1.4,
+    opacity:0,
+    y:20,
+    ease: Expo.easeInOut
+  });
+  TweenMax.from(".leftsect",2.3, {
+      delay:1.2,
+      opacity:0,
+      y:20,
+      ease: Expo.easeInOut
+  });
+  TweenMax.from(".blogtitle",2.3, {
+    delay:1.7,
+    opacity:0,
+    y:20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".blogs",2.3, {
+    delay:1.9,
+    opacity:0,
+    y:20,
+    ease: Expo.easeInOut
+});
+TweenMax.from(".titlehapp",2.3, {
+    delay:1.7,
+    opacity:0,
+    y:20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".lefthap",2.3, {
+    delay:1.9,
+    opacity:0,
+    y:20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".rightwraper",2.3, {
+    delay:1.7,
+    opacity:0,
+    y:20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".small",2.3, {
+    delay:2.9,
+    opacity:0,
+    y:20,
+    ease: Expo.easeInOut
+});
+
+ 
+ 
